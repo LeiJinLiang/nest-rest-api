3a519bf2a287a5c530d85445f3e07a139565b700
Andy.L.Jin <Andy.L.Jin@newegg.com>
2019-06-01 22:47:03 +0800

    add schema && controller && service

---
+++ src/config/keys.ts
@@ -0,0 +1,3 @@
+export default {
+    mongoURI : 'mongodb+srv://jinlei:123qwe@cluster0-7nx4s.mongodb.net/test?retryWrites=true&w=majority',
+};
---
+++ src/items/items.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { Item } from './interfaces/item.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateItemDto } from './dto/create-item.dto';
+@Injectable()
+export class ItemsService {
+    constructor(@InjectModel('Item') private readonly itemModel: Model<Item>) {}
+    async create(createItemDto: CreateItemDto): Promise<Item> {
+        const createdItemDto = new this.itemModel(createItemDto);
+        return await createdItemDto.save();
+    }
+    async findAll(): Promise<Item[]> {
+        return await this.itemModel.find().exec();
+    }
+    async findOne(id: string): Promise<Item> {
+        return await this.itemModel.findOne({ _id : id});
+    }
+    async delete(id: string): Promise<Item> {
+        return await this.itemModel.findByIdAndRemove(id);
+    }
+    async update(id: string, item: Item): Promise<Item> {
+        return await this.itemModel.findByIdAndUpdate(id, item, {new: true});
+    }
+}
