b336253a19477dc1a4c27021c242f2bb20e214ce
lzy0508 <lzy0508lzy@gmail.com>
2021-05-20 23:58:25 +0800

    test : cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsController } from './items/items.controller';
 import { ItemsService } from './items/items.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ItemModule } from './items/item.module';
+import { CoresMiddleware } from './cors.middleware';
 import config from './config/keys';
 
 @Module({
@@ -12,4 +13,10 @@ import config from './config/keys';
   controllers: [AppController, ItemsController],
   providers: [AppService, ItemsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(CoresMiddleware)
+      .forRoutes('*');
+  }
+}
